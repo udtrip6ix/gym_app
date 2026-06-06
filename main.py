e93a6260fda15e0dc9ff7f46199db0e6ae719567
Richard <villy.concombre@gmail.com>
2026-06-06 21:06:12 +1000

    main started

--- main.py
+++ main.py
@@ -1,9 +1,62 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang import Builder
+import sqlite3
+from kivy.config import Config
+from kivy.uix.boxlayout import BoxLayout
 
-class TestApp(App):
+# Настройки окна
+Config.set('graphics', 'width', '360')
+Config.set('graphics', 'height', '640')
+Config.set('graphics', 'resizable', '0')
+
+class MainScreen(BoxLayout):
+    pass
+
+def init_db():
+    conn = sqlite3.connect('tracker.db')
+    cursor = conn.cursor()
+    cursor.execute('PRAGMA journal_mode=WAL;') #для просмотра в дбивере
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS exercises (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+class TrackerApp(App):
     def build(self):
-        return Label(text="It works")
+        Builder.load_file('kv/tracker.kv')
+        self.root_widget = MainScreen()
+        self.load_exercises() 
+        return self.root_widget
+    
+    def save_exercise(self, text):
+        if not text.strip():
+            return
 
-if __name__ == "__main__":
-    TestApp().run()
+        conn = sqlite3.connect('tracker.db')
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO exercises (name) VALUES (?)', (text,))
+        conn.commit()
+        conn.close()
+        
+        print(f"Упражнение '{text}' сохранено!")
+        
+        self.root_widget.ids.exercise_name.text = ""
+        self.load_exercises()
+
+    def load_exercises(self):
+        conn = sqlite3.connect('tracker.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT name FROM exercises')
+        rows = cursor.fetchall()
+        conn.close()
+        
+
+        self.root_widget.ids.exercise_list.data = [{'text': row[0]} for row in rows]
+
+if __name__ == '__main__':
+    init_db()
+    TrackerApp().run()
