9225d6b27bc84a39f02ea3e15e2b0127c4794080
Richard <villy.concombre@gmail.com>
2026-06-11 01:17:02 +1000

    some changes...

--- main.py
+++ main.py
@@ -1,62 +1,71 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import NumericProperty, StringProperty
 import sqlite3
 from kivy.config import Config
-from kivy.uix.boxlayout import BoxLayout
 
 # Настройки окна
 Config.set('graphics', 'width', '360')
 Config.set('graphics', 'height', '640')
 Config.set('graphics', 'resizable', '0')
 
-class MainScreen(BoxLayout):
-    pass
+# Классы экранов
+class MenuScreen(Screen): pass
+class EditorScreen(Screen): pass
 
-def init_db():
-    conn = sqlite3.connect('tracker.db')
-    cursor = conn.cursor()
-    cursor.execute('PRAGMA journal_mode=WAL;') #для просмотра в дбивере
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS exercises (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL
-        )
-    ''')
-    conn.commit()
-    conn.close()
+
+class ExercisesScreen(Screen):
+    def load_exercises(self):
+        conn = sqlite3.connect('tracker.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT id, name FROM exercises')
+        rows = cursor.fetchall()
+        conn.close()
+        self.ids.exercise_list.data = [{'text': row[1], 'exercise_id': row[0]} for row in rows]
+
+
+
+class ExerciseItem(BoxLayout):
+    text = StringProperty()
+    exercise_id = NumericProperty()
+
+    def delete_exercise(self):
+        conn = sqlite3.connect('tracker.db')
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM exercises WHERE id = ?', (self.exercise_id,))
+        conn.commit()
+        conn.close()
+        App.get_running_app().root.get_screen('exercises').load_exercises()
 
 class TrackerApp(App):
     def build(self):
-        Builder.load_file('kv/tracker.kv')
-        self.root_widget = MainScreen()
-        self.load_exercises() 
-        return self.root_widget
-    
-    def save_exercise(self, text):
-        if not text.strip():
-            return
+        self.init_db()
+        return Builder.load_file('kv/tracker.kv')
 
+    def init_db(self):
+        conn = sqlite3.connect('tracker.db')
+        cursor = conn.cursor()
+        cursor.execute('PRAGMA journal_mode=WAL;')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS exercises 
+                          (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)''')
+        conn.commit()
+        conn.close()
+
+    def save_exercise(self, text):
+        if not text.strip(): return
         conn = sqlite3.connect('tracker.db')
         cursor = conn.cursor()
         cursor.execute('INSERT INTO exercises (name) VALUES (?)', (text,))
         conn.commit()
         conn.close()
-        
-        print(f"Упражнение '{text}' сохранено!")
-        
-        self.root_widget.ids.exercise_name.text = ""
-        self.load_exercises()
+
+        self.root.get_screen('exercises').load_exercises()
 
     def load_exercises(self):
-        conn = sqlite3.connect('tracker.db')
-        cursor = conn.cursor()
-        cursor.execute('SELECT name FROM exercises')
-        rows = cursor.fetchall()
-        conn.close()
-        
 
-        self.root_widget.ids.exercise_list.data = [{'text': row[0]} for row in rows]
+        self.root.get_screen('exercises').load_exercises()
 
 if __name__ == '__main__':
-    init_db()
     TrackerApp().run()
